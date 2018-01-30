c16a1086c18c1190eec24c34ec71688368d1bd07
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-30 10:26:07 +0100

    Fix

--- Plugins/escalar/jquery.escalar.js
+++ Plugins/escalar/jquery.escalar.js
@@ -1,19 +1,18 @@
 (function($){
-    $.fn.escalar = function(){
-        this.each(function(){
+    $.fn.escalar = function() {
+        this.each(function() {
             $(this).prepend("<input type='range' min='100' max='800'/><br>");
-            $(this).children().first().on("change", function(){
-            $(this).siblings().filter('img').width($(this).val());
-
+            $(this).children().first().on("change", function() {
+                $(this).siblings().filter('img').width($(this).val());
             });
         });
         return this;
     };
 
-    $.fn.difuminar = function(){
-        this.each(function(){
+    $.fn.difuminar = function() {
+        this.each(function() {
             $(this).prepend("<input type='range' min='0' max='1' step='0.1' value='0.5'/><br>");
-            $(this).children().first().on("change", function(){
+            $(this).children().first().on("change", function() {
                 $(this).siblings().filter('img').fadeTo('slow',$(this).val());
             });
         });
