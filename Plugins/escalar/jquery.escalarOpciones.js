1f31cb564d9e8ae8ee9ca4adc26cf78ba7223081
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-01-30 10:11:59 +0100

    Limpieza en código y sintaxis

--- Plugins/escalar/jquery.escalarOpciones.js
+++ Plugins/escalar/jquery.escalarOpciones.js
@@ -1,26 +1,22 @@
 (function($){
-	
-	$.fn.extend({
+    $.fn.extend({
+        escalar: function(opciones){
+            //opciones por defecto
+            var conf = {
+                min: 100,
+                max: 800
+            };
 
-		escalar: function(opciones){
+            $.extend(conf,opciones);
 
-				//opciones por defecto
-					var conf = {
-						min: 100,
-						max: 800
-					}
+            this.each(function(){
+                $(this).prepend("<input type='range' min='"+ conf.min + "' max='" +conf.max + "' /><br>");
+                $(this).children().first().on("change", function(){
+                    $(this).siblings().filter('img').width( $(this).val());
 
-					$.extend(conf,opciones);
-
-					this.each(function(){
-						$(this).prepend("<input type='range' min='"+ conf.min + "' max='" +conf.max + "' /><br>");
-						$(this).children().first().on("change", function(){
-							$(this).siblings().filter('img').width( $(this).val());
-
-						});
-					});
-					return this;
-				}
-	});
-
+                });
+            });
+            return this;
+        }
+    });
 })(jQuery);
