9325525d0d49a77c217fc78963d46dc37228c4a1
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-08 19:12:42 +0100

    Parte básica del ejercicio completado

--- Ajax/Lista Desplegable de Provincias/Servidor/cargaMunicipiosJSON.php
+++ Ajax/Lista Desplegable de Provincias/Servidor/cargaMunicipiosJSON.php
@@ -8111,7 +8111,7 @@ $municipios["51"]["0013"] = "Ceuta";
 $municipios["52"]["0018"] = "Melilla";
 
 
-$provincia = trim($_POST["provincia"]);
+$provincia = trim($_GET["provincia"]);
 $losMunicipios = $municipios[$provincia];
 
 foreach ($losMunicipios as $codigo => $nombre) {
