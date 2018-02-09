b5b77b98ddab5ad04afef84ace2f19403677d4b9
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-09 02:06:42 +0100

    Limpio adecuando a reglas de estilos

--- Ajax/5 - Ver vuelos/Servidor/consulta.php
+++ Ajax/5 - Ver vuelos/Servidor/consulta.php
@@ -1,25 +1,24 @@
 ﻿<?php
-  header('Content-type: application/json; charset=utf-8');
-  
-  class Flight   {
-        public $flightNumber;
-        public $time;  
-    }  
+header('Content-type: application/json; charset=utf-8');
 
-    $flight1 = new Flight();
-    $flight1->flightNumber = '308';
-    $flight1->time = '3 de Abril, 5:29pm';
+class Flight
+{
+    public $flightNumber;
+    public $time;
+}
 
-    $flight2 = new Flight();
-    $flight2->flightNumber = '298';
-    $flight2->time = '3 de Abril, 2:46am';
-    
-    $flight3 = new Flight();
-    $flight3->flightNumber = '421';
-    $flight3->time = '3 de Abril, 3:35pm';
+$flight1 = new Flight();
+$flight1->flightNumber = '308';
+$flight1->time = '3 de Abril, 5:29pm';
 
-    $array = array($flight1, $flight2, $flight3);
+$flight2 = new Flight();
+$flight2->flightNumber = '298';
+$flight2->time = '3 de Abril, 2:46am';
 
-    
-  echo json_encode($array);
-?>
+$flight3 = new Flight();
+$flight3->flightNumber = '421';
+$flight3->time = '3 de Abril, 3:35pm';
+
+$array = array($flight1, $flight2, $flight3);
+
+echo json_encode($array);
