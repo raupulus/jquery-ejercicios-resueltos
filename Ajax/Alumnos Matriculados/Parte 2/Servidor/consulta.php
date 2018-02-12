87a424f8e92b6380d837bb972a60c11036b86a07
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-12 19:28:15 +0100

    Adapto parte del servidor para el ejercicio

--- Ajax/Alumnos Matriculados/Parte 2/Servidor/consulta.php
+++ Ajax/Alumnos Matriculados/Parte 2/Servidor/consulta.php
@@ -7,32 +7,34 @@
         public $acceso;  
     }  
 
-    $a1 = new Alumno();
-    $a1->nombre = 'Manuel Blanca';
-    $a1->dni = '77335544A';
-    $a1->acceso = array('CFGM SMR');
-
-    $a2 = new Alumno();
-    $a2->nombre = 'María Jiménez';
-    $a2->dni = '22335544B';
-    $a2->acceso = array('CFGM SMR');
+    if ($_GET['curso'] === '1daw') {
+        $a1 = new Alumno();
+        $a1->nombre = 'Manuel Blanca';
+        $a1->dni = '77335544A';
+        $a1->acceso = array('CFGM SMR');
     
-    $primero = array($a1,$a2);
-
-    $a3 = new Alumno();
-    $a3->nombre = 'Juan Parras';
-    $a3->dni = '77665566X';
-    $a3->acceso = array('Bachillerato');
-
-    $a4 = new Alumno();
-    $a4->nombre = 'Juana Moral';
-    $a4->dni = '25335544E';
-    $a4->acceso = array('CFGS ASIR');
+        $a2 = new Alumno();
+        $a2->nombre = 'María Jiménez';
+        $a2->dni = '22335544B';
+        $a2->acceso = array('CFGM SMR');
     
-    $segundo = array($a3,$a4);
-
-    $ciclos = array($primero, $segundo);
+        $ciclo = array($a1,$a2);
+    } elseif ($_GET['curso'] === '2daw') {
+        $a3 = new Alumno();
+        $a3->nombre = 'Juan Parras';
+        $a3->dni = '77665566X';
+        $a3->acceso = array('Bachillerato');
+    
+        $a4 = new Alumno();
+        $a4->nombre = 'Juana Moral';
+        $a4->dni = '25335544E';
+        $a4->acceso = array('CFGS ASIR');
+    
+        $ciclo = array($a3,$a4);
+    }
 
     
-  echo json_encode($ciclos);
+
+    
+  echo json_encode($ciclo);
 ?>
