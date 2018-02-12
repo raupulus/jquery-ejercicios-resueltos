6197e745c299e10c774cf589ea869170493dc6d3
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-12 22:26:03 +0100

    Mejorado script

--- Ajax/Alumnos Matriculados/Parte 1/Servidor/consulta.php
+++ Ajax/Alumnos Matriculados/Parte 1/Servidor/consulta.php
@@ -1,38 +1,38 @@
 ﻿<?php
-  header('Content-type: application/json; charset=utf-8');
-  
-  class Alumno   {
-        public $nombre;
-        public $dni;  
-        public $acceso;  
-    }  
+header('Content-type: application/json; charset=utf-8');
 
-    $a1 = new Alumno();
-    $a1->nombre = 'Manuel Blanca';
-    $a1->dni = '77335544A';
-    $a1->acceso = array('CFGM SMR');
+class Alumno
+{
+    public $nombre;
+    public $dni;
+    public $acceso;
+}
 
-    $a2 = new Alumno();
-    $a2->nombre = 'María Jiménez';
-    $a2->dni = '22335544B';
-    $a2->acceso = array('CFGM SMR');
-    
-    $primero = array($a1,$a2);
+$a1 = new Alumno();
+$a1->nombre = 'Manuel Blanca';
+$a1->dni = '77335544A';
+$a1->acceso = array('CFGM SMR');
 
-    $a3 = new Alumno();
-    $a3->nombre = 'Juan Parras';
-    $a3->dni = '77665566X';
-    $a3->acceso = array('Bachillerato');
+$a2 = new Alumno();
+$a2->nombre = 'María Jiménez';
+$a2->dni = '22335544B';
+$a2->acceso = array('CFGM SMR');
 
-    $a4 = new Alumno();
-    $a4->nombre = 'Juana Moral';
-    $a4->dni = '25335544E';
-    $a4->acceso = array('CFGS ASIR');
-    
-    $segundo = array($a3,$a4);
+$primero = array($a1, $a2);
 
-    $ciclos = array($primero, $segundo);
+$a3 = new Alumno();
+$a3->nombre = 'Juan Parras';
+$a3->dni = '77665566X';
+$a3->acceso = array('Bachillerato');
 
-    
-  echo json_encode($ciclos);
-?>
+$a4 = new Alumno();
+$a4->nombre = 'Juana Moral';
+$a4->dni = '25335544E';
+$a4->acceso = array('CFGS ASIR');
+
+$segundo = array($a3, $a4);
+
+$ciclos = array($primero, $segundo);
+
+
+echo json_encode($ciclos);
