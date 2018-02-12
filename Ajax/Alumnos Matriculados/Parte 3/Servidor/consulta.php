01eb9137d56e64ccdd01fb56f22af737793ccafd
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-12 22:55:11 +0100

    Reemplazo contenido del servidor desde parte2 del ejercicio

--- Ajax/Alumnos Matriculados/Parte 3/Servidor/consulta.php
+++ Ajax/Alumnos Matriculados/Parte 3/Servidor/consulta.php
@@ -8,30 +8,30 @@ class Alumno
     public $acceso;
 }
 
-$a1 = new Alumno();
-$a1->nombre = 'Manuel Blanca';
-$a1->dni = '77335544A';
-$a1->acceso = array('CFGM SMR');
+if ($_GET['curso'] === '1daw') {
+    $a1 = new Alumno();
+    $a1->nombre = 'Manuel Blanca';
+    $a1->dni = '77335544A';
+    $a1->acceso = array('CFGM SMR');
 
-$a2 = new Alumno();
-$a2->nombre = 'María Jiménez';
-$a2->dni = '22335544B';
-$a2->acceso = array('CFGM SMR');
+    $a2 = new Alumno();
+    $a2->nombre = 'María Jiménez';
+    $a2->dni = '22335544B';
+    $a2->acceso = array('CFGM SMR');
 
-$primero = array($a1, $a2);
+    $ciclo = array($a1, $a2);
+} else if ($_GET['curso'] === '2daw') {
+    $a3 = new Alumno();
+    $a3->nombre = 'Juan Parras';
+    $a3->dni = '77665566X';
+    $a3->acceso = array('Bachillerato');
 
-$a3 = new Alumno();
-$a3->nombre = 'Juan Parras';
-$a3->dni = '77665566X';
-$a3->acceso = array('Bachillerato');
+    $a4 = new Alumno();
+    $a4->nombre = 'Juana Moral';
+    $a4->dni = '25335544E';
+    $a4->acceso = array('CFGS ASIR');
 
-$a4 = new Alumno();
-$a4->nombre = 'Juana Moral';
-$a4->dni = '25335544E';
-$a4->acceso = array('CFGS ASIR');
+    $ciclo = array($a3, $a4);
+}
 
-$segundo = array($a3, $a4);
-
-$ciclos = array($primero, $segundo);
-
-echo json_encode($ciclos);
+echo json_encode($ciclo);
