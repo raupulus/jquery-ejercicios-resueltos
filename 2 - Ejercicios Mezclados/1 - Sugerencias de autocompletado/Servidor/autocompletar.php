7da4db6c0ad0a2647d84dca7b88894bbbe06dfaa
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-21 20:27:47 +0100

    Limpio y organizo parte servidor

--- 2 - Ejercicios Mezclados/1 - Sugerencias de autocompletado/Servidor/autocompletar.php
+++ 2 - Ejercicios Mezclados/1 - Sugerencias de autocompletado/Servidor/autocompletar.php
@@ -1,5 +1,7 @@
 <?php
-// Fill up array with names
+/*
+ * Array con nombres de usuarios
+ */
 $a[]="Anna";
 $a[]="Brittany";
 $a[]="Cinderella";
@@ -31,23 +33,24 @@ $a[]="Ellen";
 $a[]="Wenche";
 $a[]="Vicky";
 
-// get the q parameter from URL
-$q=$_REQUEST["q"]; $hint="";
+// Obtiene el parámetro desde la URL
+$q=$_REQUEST["q"];
+$hint="";
 
-// lookup all hints from array if $q is different from "" 
-if ($q !== "")
-  { $q=strtolower($q); $len=strlen($q);
-    foreach($a as $name)
-    { if (stristr($q, substr($name,0,$len)))
-      { if ($hint==="")
-        { $hint=$name; }
-        else
-        { $hint .= ", $name"; }
-      }
+// Comprueba con todos los elementos del array si $q no es cadena vacía: ""
+if ($q !== "") {
+    $q=strtolower($q); $len=strlen($q);
+
+    foreach($a as $name) {
+        if (stristr($q, substr($name,0,$len))) {
+            if ($hint==="") {
+                $hint=$name;
+            } else {
+                $hint .= ", $name";
+            }
+        }
     }
-  }
+}
 
-// Output "no suggestion" if no hint were found
-// or output the correct values 
-echo $hint==="" ? "no suggestion" : $hint;
-?>
+// Devuelve "no suggestion" si no es encontrado o no hay valor correcto
+echo $hint==="" ? "Sin sugerencias" : $hint;
