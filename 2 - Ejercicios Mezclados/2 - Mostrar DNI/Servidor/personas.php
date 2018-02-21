91bb5988c9e51782f354d9b2947afdca38511256
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-02-21 20:00:22 +0100

    Mostrar dni plantilla base

---
+++ 2 - Ejercicios Mezclados/2 - Mostrar DNI/Servidor/personas.php
@@ -0,0 +1,10 @@
+<?php
+	$datos=array(
+		0=>array("id"=>1,"dni"=>"12345678-a", "nombre"=>"Juan"),
+		1=>array("id"=>2,"dni"=>"23143543-d", "nombre"=>"Antonio"),
+		2=>array("id"=>3,"dni"=>"75770999-d", "nombre"=>"Pepa"),
+		3=>array("id"=>4,"dni"=>"75754321-f", "nombre"=>"Pepe")
+		
+	);
+	echo json_encode($datos);
+?>
